efe700d1f628e6c6e404cbee20608b182e75552c
Jminami <mj14rugby@yahoo.co.jp>
2023-08-06 11:25:28 +0900

    configの型を指定

--- scripts/config.ts
+++ scripts/config.ts
@@ -1,1 +1,7 @@
-export const config = require("config")
+const values = ["openAiApiKey", "apiModel"] as const
+type ValueType = (typeof values)[number]
+interface Config {
+  get: (key: ValueType) => any
+  has: (key: ValueType) => boolean
+}
+export const config: Config = require("config")
